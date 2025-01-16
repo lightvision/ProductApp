0da64378fd2fdd4821d69101998fcce4cf5c9e37
Marius Ionel <marius.ionel@s.utm.ro>
2025-01-16 20:10:07 +0200

    Finished CRUD App Added screnshots from runtime

--- src/main/java/routmmariusionel/productapp/ProductApp.java
+++ src/main/java/routmmariusionel/productapp/ProductApp.java
@@ -1,3 +1,18 @@
+/**
+ * ProductApp
+ * Această aplicație JavaFX permite gestionarea produselor utilizând o interfață grafică conectată la o bază de date SQLite.
+ * Funcționalități principale:
+ * - Adăugarea unui produs (nume și preț).
+ * - Vizualizarea produselor într-un tabel.
+ * - Actualizarea informațiilor despre un produs selectat.
+ * - Ștergerea unui produs selectat, cu confirmare prealabilă.
+ *
+ * Aplicația include validări pentru datele introduse și notificări pentru utilizator în cazul erorilor.
+ *
+ * Pentru detalii de rulare se pot vedea si screenshot-urile atasate
+ *
+ * @copyright Marius Ionel, Informatica ID, anul 3, promotia 2022 - 2025
+ */
 package routmmariusionel.productapp;
 
 import javafx.application.Application;
